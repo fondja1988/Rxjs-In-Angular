596da69020206e3f3048e5cf33b694f0485df729
fondja1988 <narcissefondja0@gmail.com>
2023-05-12 00:05:07 +0200

    of and from observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = `Learning RXJS in Angular`;
   constructor() {
-    console.log('je suis dans la console de stackblitz')
+    of(4, 1, 9).subscribe(
+      {
+        next: item => console.log(item)
+      }
+    );
+    from([7, 6, 0]).subscribe({
+      next: item => console.log(`from observable ${item}`)
+    });
+    of(...['narcisse', 'carvalo', 'johanna', 'chloe']).subscribe(
+    {
+      next: el => console.log(el),
+      error: err => console.log(err),
+      complete: () => console.log(`the fine`)
+    }
+    )
   }
 }
